83dbf486cb6c9f11741a6311f5bf9420fc988902
Prabhakar Mishra <prabhakar.2125cs1183@kiet.edu>
2023-01-19 20:19:04 +0530

    initital commits

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .views import RegisterUser,LoginUser,logout_view
+from .views import *
+from .templates import *
 from django.urls import path,include
 
 urlpatterns = [
     path('register/', RegisterUser),
     path('login/', LoginUser),
     path('logout/', logout_view),
+    path('login_2/', mylogin ),
+    path('addtodo/', addTodo ),
     
     # path('',include('home.urls')),
 ]
